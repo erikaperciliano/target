1540b79a262630d847e4a6079316b0514122a89f
Erika Perciliano <erikaperciliano.developer@gmail.com>
2026-08-31 17:44:26 -0300

    feat: creates Separator Component

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -6,7 +6,7 @@ import { Pressable, Text, View } from "react-native";
 export default function Index() {
   return (
     <View style={{ flex: 1 }}>
-      <HomeHeader />
+      <HomeHeader data={{ total: 'R$ 2.680, 00'}}/>
     </View>
   );
 }
--- src/components/HomeHeader/index.tsx
+++ src/components/HomeHeader/index.tsx
@@ -3,8 +3,17 @@ import { LinearGradient } from "expo-linear-gradient";
 import { styles } from "./styles";
 import { colors } from "@/theme/colors";
 import { View, Text } from "react-native";
+import { Separator } from "../Separator";
 
-export function HomeHeader() {
+export type HomeHeaderProps = {
+    total: string
+}
+
+type Props = {
+    data: HomeHeaderProps
+}
+
+export function HomeHeader({ data }: Props) {
     return (
         <LinearGradient
             colors={[colors.blue[500], colors.blue[800]]}
@@ -12,7 +21,10 @@ export function HomeHeader() {
         >
             <View>
                 <Text style={styles.label}>Total que você possui</Text>
+                <Text style={styles.total}>{data.total}</Text>
             </View>
+
+            <Separator color={colors.blue[400]}/>
         </LinearGradient>
     )
 }
