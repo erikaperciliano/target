eccedf6b1fdc95e4240bfff275e9658566cd1525
Erika Perciliano <erikaperciliano.developer@gmail.com>
2026-08-31 18:45:17 -0300

    feat: creates Summary Component

--- src/components/HomeHeader/index.tsx
+++ src/components/HomeHeader/index.tsx
@@ -4,6 +4,7 @@ import { styles } from "./styles";
 import { colors } from "@/theme/colors";
 import { View, Text } from "react-native";
 import { Separator } from "../Separator";
+import { Summary } from "../Summary";
 
 export type HomeHeaderProps = {
     total: string
@@ -25,6 +26,19 @@ export function HomeHeader({ data }: Props) {
             </View>
 
             <Separator color={colors.blue[400]}/>
+
+            <View style={styles.summary}>
+                <Summary
+                    data={{ label: 'Entradas', value: 'R$6.184.90' }}
+                    icon={{ name: 'arrow-upward', color: colors.green[500] }}
+                />
+
+                <Summary
+                    isLeft
+                    data={{ label: 'Saídas', value: '-R$883.65' }}
+                    icon={{ name: 'arrow-downward', color: colors.red[400] }}
+                />
+            </View>
         </LinearGradient>
     )
 }
