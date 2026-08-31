d9e392a726c5ec26d9dfe8000423c796134693d0
Erika Perciliano <erikaperciliano.developer@gmail.com>
2026-08-31 12:50:19 -0300

    feat: adds Layout

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,14 +1,22 @@
 import { router } from "expo-router";
-import { Button, Text, View } from "react-native";
+import { Pressable, Text, View } from "react-native";
 
 export default function Index() {
-    return (
-        <View style={{ flex: 1, justifyContent: 'center'}}>
-            <Text>Expo Router</Text>
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center", gap: 16 }}>
+      <Text style={{ fontSize: 20, fontWeight: "bold", marginBottom: 10 }}>Expo Router</Text>
 
-            <Button title="Nova Meta" onPress={() => router.navigate('/target')} />
-            <Button title="Transação" onPress={() => router.navigate('/transaction/132')} />
-            <Button title="Progresso" onPress={() => router.navigate('/in-progress/12')} />
-        </View>
-    )
+      <Pressable onPress={() => router.navigate('/target')}>
+        <Text style={{ color: "#007AFF", fontSize: 16 }}>Nova Meta</Text>
+      </Pressable>
+
+      <Pressable onPress={() => router.navigate('/transaction/132')}>
+        <Text style={{ color: "#007AFF", fontSize: 16 }}>Transação</Text>
+      </Pressable>
+
+      <Pressable onPress={() => router.navigate('/in-progress/12')}>
+        <Text style={{ color: "#007AFF", fontSize: 16 }}>Progresso</Text>
+      </Pressable>
+    </View>
+  );
 }
