a7cad2ac24f9456c16ec399e31639839a4547bc0
Erika Perciliano <erikaperciliano.developer@gmail.com>
2026-08-31 12:30:35 -0300

    feat: passing parameters to route

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,9 +1,13 @@
-import { Text, View } from "react-native";
+import { router } from "expo-router";
+import { Button, Text, View } from "react-native";
 
 export default function Index() {
     return (
         <View style={{ flex: 1, justifyContent: 'center'}}>
             <Text>Expo Router</Text>
+
+            <Button title="Nova Meta" onPress={() => router.navigate('/target')} />
+            <Button title="Transação" onPress={() => router.navigate('/transaction/132')} />
         </View>
     )
 }
