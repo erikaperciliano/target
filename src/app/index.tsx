648ed8c281a14b5aaf88f2b29f5157333fa84dee
Erika Perciliano <erikaperciliano.developer@gmail.com>
2026-08-31 12:36:02 -0300

    feat: creates In Progress route

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -8,6 +8,7 @@ export default function Index() {
 
             <Button title="Nova Meta" onPress={() => router.navigate('/target')} />
             <Button title="Transação" onPress={() => router.navigate('/transaction/132')} />
+            <Button title="Progresso" onPress={() => router.navigate('/in-progress/12')} />
         </View>
     )
 }
