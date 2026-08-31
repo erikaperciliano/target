fbda5b5d5a9e7be9c5e940dae7d884cace25198a
Erika Perciliano <erikaperciliano.developer@gmail.com>
2026-08-31 17:35:05 -0300

    feat: unifying the import of tokens, colors, and fonts

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,23 +1,12 @@
+import { HomeHeader } from "@/components/HomeHeader";
 import { fontFamily } from "@/theme/fontFamily";
 import { router } from "expo-router";
 import { Pressable, Text, View } from "react-native";
 
 export default function Index() {
   return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center", gap: 16 }}>
-      <Text style={{ fontFamily: fontFamily.bold, fontSize: 20, fontWeight: "bold", marginBottom: 10 }}>Expo Router</Text>
-
-      <Pressable onPress={() => router.navigate('/target')}>
-        <Text style={{ color: "#007AFF", fontSize: 16 }}>Nova Meta</Text>
-      </Pressable>
-
-      <Pressable onPress={() => router.navigate('/transaction/132')}>
-        <Text style={{ color: "#007AFF", fontSize: 16 }}>Transação</Text>
-      </Pressable>
-
-      <Pressable onPress={() => router.navigate('/in-progress/12')}>
-        <Text style={{ color: "#007AFF", fontSize: 16 }}>Progresso</Text>
-      </Pressable>
+    <View style={{ flex: 1 }}>
+      <HomeHeader />
     </View>
   );
 }
---
+++ src/components/HomeHeader/index.tsx
@@ -0,0 +1,18 @@
+import { LinearGradient } from "expo-linear-gradient";
+
+import { styles } from "./styles";
+import { colors } from "@/theme/colors";
+import { View, Text } from "react-native";
+
+export function HomeHeader() {
+    return (
+        <LinearGradient
+            colors={[colors.blue[500], colors.blue[800]]}
+            style={styles.container}
+        >
+            <View>
+                <Text style={styles.label}>Total que você possui</Text>
+            </View>
+        </LinearGradient>
+    )
+}
