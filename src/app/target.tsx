8b7420cecf981d9de784f8b410b30abf65ee1dcb
Erika Perciliano <erikaperciliano.developer@gmail.com>
2026-09-01 17:41:14 -0300

    feat: creates Input Component

--- src/app/target.tsx
+++ src/app/target.tsx
@@ -1,6 +1,8 @@
+import { Button } from "@/components/Button";
+import { Input } from "@/components/Input";
 import { PageHeader } from "@/components/PageHeader";
 import { router } from "expo-router";
-import { Button, Text, View } from "react-native";
+import { Text, View } from "react-native";
 
 export default function Target() {
     return (
@@ -9,7 +11,15 @@ export default function Target() {
                 title='Meta'
                 subtitle="Economizar para alcançar sua meta financeira."
             />
-            <Button title="Voltar" onPress={() => router.back()} />
+            
+            <View style={{ marginTop: 32, gap: 24 }}>
+                <Input
+                    label="Nome da meta"
+                    placeholder="Ex: Viagem para praia, Apple Watch"
+                />
+                <Button title="Salvar" />
+            </View>
+
         </View>
     )
 }
