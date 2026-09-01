d8a15929627a3c4223ae07df06d45e9900698fc8
Erika Perciliano <erikaperciliano.developer@gmail.com>
2026-09-01 15:42:22 -0300

    feat: creates Button Component

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,7 +1,9 @@
+import { Button } from "@/components/Button";
 import { HomeHeader } from "@/components/HomeHeader";
 import { List } from "@/components/List";
 import { Target } from "@/components/Target";
 import { colors } from "@/theme";
+import { router } from "expo-router";
 
 import { View } from "react-native";
 
@@ -44,10 +46,19 @@ export default function Index() {
         title="Metas"
         data={targets}
         keyExtractor={(item) => item.id}
-        renderItem={({ item }) => <Target data={item} />}
+        renderItem={({ item }) => (
+          <Target
+            data={item}
+            onPress={() => router.navigate(`/in-progress/${item.id}`)}
+          />
+        )}
         emptyMessage="Nenhuma meta. Clique em nova meta para criar."
         containerStyle={{ paddingHorizontal: 24 }}
       />
+
+      <View style={{ padding: 24, paddingBottom: 32}}>
+        <Button title="Nova meta" onPress={() => router.navigate('/target')} />
+      </View>
     </View>
   );
 }
