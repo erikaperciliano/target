8cf1ffb408c9de8fbb10e9a4a60ebe3ae023140c
Erika Perciliano <erikaperciliano.developer@gmail.com>
2026-09-03 14:48:17 -0300

    feat: creates transactions database

--- src/database/migrate.ts
+++ src/database/migrate.ts
@@ -2,7 +2,7 @@ import { SQLiteDatabase } from "expo-sqlite";
 
 export async function migrate(database: SQLiteDatabase) {
     await database.execAsync(`
-        PRAGMA foreing_key = ON;
+        PRAGMA foreing_keys = ON;
         
         CREATE TABLE IF NOT EXISTS targets (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -11,5 +11,18 @@ export async function migrate(database: SQLiteDatabase) {
             created_at timestamp NOT NULL DEFAULT current_timestamp,
             updated_at timestamp NOT NULL DEFAULT current_timestamp,
         );
-    `);
+
+        CREATE TABLE IF NOT EXISTS  transactions (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            target_id  INTEGER NOT NULL,
+            amount FLOAT NOT NULL,
+            observation TEXT NULL,
+            created_at timestamp NOT NULL DEFAULT current_timestamp,
+            updated_at timestamp NOT NULL DEFAULT current_timestamp,
+
+            CONSTRANT fk_targets_transactions
+            FOREING KEY (target_id) REFERENCES targets(id)
+            ON DELETE CASCADE
+        );
+    `)
 }
