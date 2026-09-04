6de3b7597a17890cd564725dc659f85a79ad4e12
Erika Perciliano <erikaperciliano.developer@gmail.com>
2026-09-04 19:38:03 -0300

    feat: removes transaction

--- src/database/useTransactionsDatabase.ts
+++ src/database/useTransactionsDatabase.ts
@@ -42,9 +42,15 @@ export function useTransactionsDatabase() {
         `)
     }
 
+     async function remove(id: number) {
+       await database.runAsync('DELETE FROM transactions WHERE id = ?', id)
+    }
+    
+
     return {
         create,
         listByTargetId,
+        remove,
     }
 }
 
