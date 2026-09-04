c93916efe58027ca120794515f9a4ccb6b213354
Erika Perciliano <erikaperciliano.developer@gmail.com>
2026-09-04 19:09:01 -0300

    feat: adds transaction at database

--- src/app/transaction/[id].tsx
+++ src/app/transaction/[id].tsx
@@ -4,13 +4,48 @@ import { Input } from "@/components/Input";
 import { PageHeader } from "@/components/PageHeader";
 import { TransactionType } from "@/components/TransactionType";
 import { TransactionTypes } from "@/components/utils/TransactionTypes";
-import { useLocalSearchParams } from "expo-router";
+import { useTransactionsDatabase } from "@/database/useTransactionsDatabase";
+import { router, useLocalSearchParams } from "expo-router";
 import { useState } from "react";
-import { Text, View } from "react-native";
+import { Alert, Text, View } from "react-native";
 
 export default function Transaction() {
-    const [ type, setType ] = useState(TransactionTypes.Input)
+    const [amount, setAmount] = useState(0)
+    const [type, setType] = useState(TransactionTypes.Input)
+    const [isCreating, setIsCreating] = useState(false)
+    const [observation, setObservation] = useState('')
+    
     const params = useLocalSearchParams<{ id: string }>()
+
+    const transactionsDatabase = useTransactionsDatabase()
+
+    async function handleCreate() {
+        try {
+            if (amount <= 0) {
+                return Alert.alert('Atenção!', "Preencha o valor. A transação deve ser maior que zero.")
+            }
+
+            setIsCreating(true)
+
+            await transactionsDatabase.create({
+                target_id: Number(params.id),
+                amount: type === TransactionTypes.Output ? amount * -1 : amount,
+                observation
+            })
+
+            Alert.alert('Sucesso', 'Transação salva com sucesso!', [
+                {
+                    text: 'OK',
+                    onPress: () => router.back()
+                }
+            ])
+
+        } catch (erro) {
+            Alert.alert('Erro', 'Não foi possível salvar a transação.')
+            console.log(erro)
+            setIsCreating(false)
+        }
+    }
 
     return (
         <View style={{ flex: 1, padding: 24 }}>
@@ -21,13 +56,18 @@ export default function Transaction() {
 
             <View style={{ marginTop: 32, gap: 24 }}>
                 <TransactionType selected={type} onChange={setType} />
-                <CurrencyInput label="Valor (R$)" value={0} />
+                <CurrencyInput
+                    label="Valor (R$)"
+                    value={amount}
+                    onChangeValue={(value) => setAmount(value ?? 0)}
+                />
                 <Input
                     label="Motivo (opcional)"
                     placeholder="Ex: Investir em CDB de 110% no banco XPTO"
+                    onChangeText={setObservation}
                 />
 
-                <Button title="Salvar" />
+                <Button title="Salvar" onPress={handleCreate} isProcessing={isCreating} />
             </View>
         </View>
     )
