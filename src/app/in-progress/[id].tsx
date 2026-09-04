8cd9f1df0be7dd04c1207d435b8324b6b4f8c77a
Erika Perciliano <erikaperciliano.developer@gmail.com>
2026-09-04 17:27:18 -0300

    feat: shows goal

--- src/app/in-progress/[id].tsx
+++ src/app/in-progress/[id].tsx
@@ -1,4 +1,4 @@
-import { View } from "react-native";
+import { Alert, View } from "react-native";
 
 import { List } from "@/components/List";
 import { PageHeader } from "@/components/PageHeader";
@@ -6,13 +6,12 @@ import { Progress } from "@/components/Progress";
 import { Transaction, TransactionProps } from "@/components/Transaction";
 import { TransactionTypes } from "@/components/utils/TransactionTypes";
 import { Button } from "@/components/Button";
-import { router, useLocalSearchParams } from "expo-router";
+import { router, useFocusEffect, useLocalSearchParams } from "expo-router";
+import { useTargetDatabase } from "@/database/useTargetDatabase";
+import { useCallback, useState } from "react";
+import { numberToCurrency } from "@/components/utils/numberToCurrency";
+import { Loading } from "@/components/Loading";
 
-const details = {
-    current: 'R$ 580,00',
-    target: 'R$ 1.790,00',
-    percentage: 50,
-}
 
 const transactions: TransactionProps[] = [
     {
@@ -22,7 +21,7 @@ const transactions: TransactionProps[] = [
         type: TransactionTypes.Input,
     },
      {
-        id: '1',
+        id: '2',
         value: 'R$ 300,00',
         date: '12/04/25',
         description: 'CDB de 110% no banco XPTO',
@@ -31,12 +30,55 @@ const transactions: TransactionProps[] = [
 ]
 
 export default function InProgress() {
+    const [isFetching, setIsFetching] = useState(true)
+    const [details, setDetails] = useState({
+        name: '',
+        current: 'R$ 0,00',
+        target: 'R$ 0,00',
+        percentage: 0
+        ,
+    })
+    
     const params = useLocalSearchParams<{ id: string }>()
+
+    const targetDatabase = useTargetDatabase()
+
+    async function fetchDetails() {
+        try {
+            const response = await targetDatabase.show(Number(params.id))
+            setDetails({
+                name: response?.name ?? '',
+                current: numberToCurrency(response?.current ?? 0),
+                target: numberToCurrency(response?.amount ?? 0),
+                percentage: response?.percentage ?? 0
+            })
+        } catch (error) {
+            Alert.alert('Erro', 'Não foi possível carregar os detalhes da meta.')
+            console.log(error)
+        }
+    }
+
+    async function fetchData() {
+        const fetchDetailsPromise = fetchDetails()
+
+        await Promise.all([fetchDetailsPromise])
+        setIsFetching(false)
+    }
+
+    useFocusEffect(
+        useCallback(() => {
+            fetchData()
+        }, [])
+    )
+
+    if (isFetching) {
+        return <Loading />
+    }
 
     return (
         <View style={{ flex: 1, padding: 24, gap: 32 }}>
             <PageHeader
-                title="Apple Watch"
+                title={details.name}
                 rightButton={{
                     icon: 'edit',
                     onPress: () => {}
